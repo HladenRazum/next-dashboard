0c2c849c6c5d85600ea783e78e254de4201ec147
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2024-06-28 11:59:28 +0300

    added i18n

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,12 @@
 import Example from '../components/Example';
+import initTranslations from '../i18n';
 
-export default function Page() {
+export default async function Page({ params: { locale } }) {
+  const { t } = await initTranslations(locale, ['test']);
+
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <h2>{t('welcome')}</h2>
       <Example />
     </main>
   );
